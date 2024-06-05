0f0eed84fd35b78d6ae4a2ce6adc1ccb8317c0e4
Jamie Chang <jamiechang917@gmail.com>
2024-06-05 11:28:34 +0800

    Main update

---
+++ fargo3d/data.py
@@ -0,0 +1,246 @@
+import os
+import numpy as np
+import astropy.units as u
+import astropy.constants as const
+
+class Data():
+    def __init__(self, path:str, snap:int, n_dust:int, unit_length:u.Unit, unit_mass:u.Unit):
+        self.path = path
+        self.snap = snap
+        self.n_dust = n_dust
+        self._check_files()
+        self.params = self._get_params()
+        self.units = self._get_units(unit_length, unit_mass)
+        self.coords = self._get_coords(ghost_cells=3) # 'x': azimuthal, 'y': radial, 'z': coaltitude
+        self.mesh   = self._get_mesh()
+        self.fluids = self._get_fluids()
+
+    # private methods
+    def _check_files(self) -> None:
+        """
+        Check the required files in the directory and change the path to the directory
+        """
+        # check if dir exists
+        if not os.path.exists(self.path):
+            raise FileNotFoundError(f"Directory {self.path} not found")
+        # check domain_x.dat, domain_y.dat, domain_z.dat
+        files = ["domain_x.dat", "domain_y.dat", "domain_z.dat", "variables.par"]
+        for file in files:
+            if not os.path.exists(os.path.join(self.path, file)):
+                raise FileNotFoundError(f"File {file} not found in {self.path}")
+        os.chdir(self.path)
+        return
+
+    def _get_params(self) -> dict:
+        """
+        Read variables.par and planet0.dat file and return the parameters as a dictionary
+        Return: dict
+        """
+        params = {}
+        with open("variables.par", "r") as f:
+            for l in f.readlines():
+                l = l.split("\t")
+                params[l[0]] = l[1].strip()
+        with open("planet0.dat", "r") as f:
+            keys = ["index", "x", "y", "z", "vx", "vy", "vz", "mass", "date", "frame_rotation_rate"]
+            planet_params = {key: [] for key in keys}
+            for l in f.readlines():
+                l = l.split("\t")
+                for i, v in enumerate(keys):
+                    if i == 0: # index
+                        planet_params[v].append(int(l[i]))
+                    else:
+                        planet_params[v].append(float(l[i]))
+            params["planet0"] = planet_params
+        return params
+    
+    def _get_units(self, unit_length:u.Unit, unit_mass:u.Unit) -> dict:
+        """
+        Calculate the units
+        """
+        units = {}
+        units["unit_mass"] = unit_mass.to(u.M_sun)
+        units["unit_length"] = unit_length.to(u.au)
+        units["unit_time"] = np.sqrt((unit_length**3 / (const.G * unit_mass))).to(u.s)
+        units["unit_rho"] = (units["unit_mass"] / units["unit_length"]**3).to(u.g / u.cm**3)
+        units["unit_sigma"] = (units["unit_mass"] / units["unit_length"]**2).to(u.g / u.cm**2)
+        units["unit_velocity"] = (units["unit_length"] / units["unit_time"]).to(u.m / u.s)
+        units["unit_time_onesnap"] = (units["unit_time"] * int(self.params["NINTERM"]) * float(self.params["DT"])).to(u.yr)
+        return units
+
+    def _get_coords(self, ghost_cells=3) -> dict:
+        """
+        Read domain files in the current directory and return the grids
+        ghost_cells: int, number of ghost cells (default=3)
+        Return: dict, keys: x, y, z; values: numpy array
+        """
+        domains = {'x': [], 'y': [], 'z': []} # azimuthal, radial, coaltitude
+        for i in domains.keys():
+            with open(f"domain_{i}.dat", "r") as f:
+                for l in f.readlines():
+                    domains[i].append(float(l))
+            if i in {'y', 'z'}:
+                domains[i] = domains[i][ghost_cells:-ghost_cells]
+            domains[i] = np.array(domains[i])
+        # convert coords to center of cells
+        coords = {'sph_phi': domains['x'], 'sph_r': domains['y'], 'sph_theta': domains['z']}
+        coords['sph_phi'] = ((coords['sph_phi'][1:] + coords['sph_phi'][:-1]) / 2) * u.rad
+        coords['sph_r'] = ((coords['sph_r'][1:] + coords['sph_r'][:-1]) / 2) * self.units["unit_length"]
+        coords['sph_theta'] = ((coords['sph_theta'][1:] + coords['sph_theta'][:-1]) / 2) * u.rad
+        return coords
+
+    def _get_mesh(self) -> dict:
+        """
+        Calculate the mesh grid
+        Returns: dict, keys: spherical, cylindrical; values: dict, keys: phi, r, theta/z; values: numpy array
+        """
+        # check coordinates
+        coordinates = self.params["COORDINATES"]
+        if coordinates != "spherical":
+            raise NotImplementedError("Only the simulation data using spherical coordinates are supported")        
+        SPH_THETA, SPH_R, SPH_PHI = np.meshgrid(self.coords['sph_theta'], self.coords['sph_r'], self.coords['sph_phi'], indexing='ij')
+        CYL_PHI, CLY_R, CLY_Z = SPH_PHI, SPH_R*np.sin(SPH_THETA), SPH_R*np.cos(SPH_THETA)
+        mesh = {"sph_phi": SPH_PHI, "sph_r": SPH_R, "sph_theta": SPH_THETA, "cyl_phi": CYL_PHI, "cyl_r": CLY_R, "cyl_z": CLY_Z}
+        return mesh
+
+    def _get_fluids(self) -> dict:
+        """
+        """
+        NX, NY, NZ = int(self.params["NX"]), int(self.params["NY"]), int(self.params["NZ"]) # azimuthal, radial, coaltitude
+        fluids_density, fluids_velocity = {}, {}
+        # density
+        fluids_density["gas"] = np.fromfile(f"gasdens{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_rho"]
+        for i in range(1, self.n_dust+1):
+            fluids_density[f"dust{i}"] = np.fromfile(f"dust{i}dens{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_rho"]
+        # velocity
+        fluids_velocity["gas"] = {
+            'x': np.fromfile(f"gasvx{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_velocity"],
+            'y': np.fromfile(f"gasvy{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_velocity"],
+            'z': np.fromfile(f"gasvz{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_velocity"]
+        }
+        for i in range(1, self.n_dust+1):
+            fluids_velocity[f"dust{i}"] = {
+                'x': np.fromfile(f"dust{i}vx{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_velocity"],
+                'y': np.fromfile(f"dust{i}vy{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_velocity"],
+                'z': np.fromfile(f"dust{i}vz{self.snap}.dat").reshape(NZ, NY, NX) * self.units["unit_velocity"]
+            }
+        # ======================================== #
+        flaring_index = float(self.params["FLARINGINDEX"])
+        aspect_ratio = float(self.params["ASPECTRATIO"])
+        sigmaslope = float(self.params["SIGMASLOPE"])
+        xi = sigmaslope + 1.0 + flaring_index
+        beta = 1.0 - 2*flaring_index
+
+        omega = self.params["planet0"]["frame_rotation_rate"][self.snap] / self.units["unit_time"]
+        h = aspect_ratio * pow((self.mesh["sph_r"] / self.units["unit_length"]), flaring_index)
+        
+        star_mass = self.units["unit_mass"]
+        vkep = np.sqrt(const.G * star_mass / self.mesh['sph_r']).to('m/s') # keplerian velocity
+        
+
+        for fluid in fluids_velocity.keys():
+            fluids_velocity[fluid]["sph_phi"]   = (fluids_velocity[fluid]["x"] + self.mesh["sph_r"] * omega * np.sin(self.mesh["sph_theta"])) / np.sqrt(pow(np.sin(self.mesh["sph_theta"]), -2 * flaring_index) - (beta+xi)*h*h)
+            fluids_velocity[fluid]["sph_r"]     = fluids_velocity[fluid]["y"]
+            fluids_velocity[fluid]["sph_theta"] = fluids_velocity[fluid]["z"]
+            fluids_velocity[fluid]["cyl_phi"]   = fluids_velocity[fluid]["sph_phi"]
+            fluids_velocity[fluid]["cyl_r"]     = fluids_velocity[fluid]["sph_r"] * np.sin(self.mesh["sph_theta"]) + fluids_velocity[fluid]["sph_theta"] * np.cos(self.mesh["sph_theta"])
+            fluids_velocity[fluid]["cyl_z"]     = - fluids_velocity[fluid]["sph_theta"] * np.sin(self.mesh["sph_theta"]) + fluids_velocity[fluid]["sph_r"] * np.cos(self.mesh["sph_theta"])
+            fluids_velocity[fluid]["vkep"]      = vkep
+        return {'density': fluids_density, 'velocity': fluids_velocity}
+    
+    # public methods
+    def get_velocity_profiles(self, r_size:int, zr_min:float, zr_max:float,
+                              phi_min=-np.pi, phi_max=np.pi, fluid='gas', coordinate='sph') -> dict:
+        """
+        Get radial velocity profile of the fluid
+        r_size:     int, number of radial bins (number of points in the r direction)
+        zr_min:     float, minimum z/r
+        zr_max:     float, maximum z/r
+        phi_min:    float, minimum phi (default=-pi)
+        phi_max:    float, maximum phi (default=pi)
+        fluid:      str, fluid name (default='gas')
+        coordinate: str, coordinate system (default='sph', options: 'sph', 'cyl')
+        """
+        if coordinate not in {'sph', 'cyl'}:
+            raise ValueError("coordinate should be either 'sph' or 'cyl'")
+        if fluid not in self.fluids['density'].keys():
+            raise ValueError(f"fluid {fluid} not found")
+        if phi_min < -np.pi or phi_max > np.pi:
+            raise ValueError("phi_min, phi_max should be within [-pi, pi]")
+        
+        zr_mesh = self.mesh['cyl_z'] / self.mesh['cyl_r'] # z/r mesh
+        mask = (zr_mesh >= zr_min) & (zr_mesh <= zr_max) & (self.mesh['cyl_phi'].value >= phi_min) & (self.mesh['cyl_phi'].value <= phi_max)
+    
+        # get r-vr, r-vtheta, r-vphi profiles
+        if coordinate == 'sph':
+            r_masked        = self.mesh['sph_r'][mask]
+            vphi_masked     = self.fluids['velocity'][fluid]['sph_phi'][mask]
+            dvphi_masked    = (self.fluids['velocity'][fluid]['sph_phi'] - self.fluids['velocity'][fluid]['vkep'])[mask]
+            vr_masked       = self.fluids['velocity'][fluid]['sph_r'][mask]
+            vtheta_masked   = self.fluids['velocity'][fluid]['sph_theta'][mask]
+            r, vphi, dvphi, vr, vtheta, r_err, vphi_err, dvphi_err, vr_err, vtheta_err = np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size)
+            r_bounds = np.linspace(np.log10(r_masked.to('au').value).min(), np.log10(r_masked.to('au').value).max(), r_size+1, endpoint=True)
+            r_bounds = 10**r_bounds * u.au
+            for i in range(r_size):                
+                mask_r = (r_masked >= r_bounds[i]) & (r_masked < r_bounds[i+1])
+                # if all the values are masked, skip
+                if mask_r.sum() == 0:
+                    raise ValueError(f"Oversampling! Please decrease 'r_size' or check the mask condition")
+                r[i] = np.mean(r_masked[mask_r].to('au').value)
+                vphi[i] = np.mean(vphi_masked[mask_r].to('m/s').value)
+                dvphi[i] = np.mean(dvphi_masked[mask_r].to('m/s').value)
+                vr[i] = np.mean(vr_masked[mask_r].to('m/s').value)
+                vtheta[i] = np.mean(vtheta_masked[mask_r].to('m/s').value)
+                r_err[i] = np.std(r_masked[mask_r].to('au').value)
+                vphi_err[i] = np.std(vphi_masked[mask_r].to('m/s').value)
+                dvphi_err[i] = np.std(dvphi_masked[mask_r].to('m/s').value)
+                vr_err[i] = np.std(vr_masked[mask_r].to('m/s').value)
+                vtheta_err[i] = np.std(vtheta_masked[mask_r].to('m/s').value)
+            return {'r': r * u.au, 'vphi': vphi * u.m/u.s, 'dvphi': dvphi * u.m/u.s, 'vr': vr * u.m/u.s, 'vtheta': vtheta * u.m/u.s,
+                    'r_err': r_err * u.au, 'vphi_err': vphi_err * u.m/u.s, 'dvphi_err': dvphi_err * u.m/u.s, 'vr_err': vr_err * u.m/u.s, 'vtheta_err': vtheta_err * u.m/u.s}
+        else:
+            r_masked        = self.mesh['cyl_r'][mask]
+            vphi_masked     = self.fluids['velocity'][fluid]['cyl_phi'][mask]
+            dvphi_masked    = (self.fluids['velocity'][fluid]['cyl_phi'] - self.fluids['velocity'][fluid]['vkep'])[mask]
+            vr_masked       = self.fluids['velocity'][fluid]['cyl_r'][mask]
+            vz_masked       = self.fluids['velocity'][fluid]['cyl_z'][mask]
+            r, vphi, dvphi, vr, vz, r_err, vphi_err, dvphi_err, vr_err, vz_err = np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size), np.zeros(r_size)
+            r_bounds = np.linspace(np.log10(r_masked.to('au').value).min(), np.log10(r_masked.to('au').value).max(), r_size+1, endpoint=True)
+            r_bounds = 10**r_bounds * u.au
+            for i in range(r_size):
+                mask_r = (r_masked >= r_bounds[i]) & (r_masked < r_bounds[i+1])
+                # if all the values are masked, skip
+                if mask_r.sum() == 0:
+                    raise ValueError(f"Oversampling! Please decrease 'r_size' or check the mask condition")
+                r[i] = np.mean(r_masked[mask_r].to('au').value)
+                vphi[i] = np.mean(vphi_masked[mask_r].to('m/s').value) 
+                dvphi[i] = np.mean(dvphi_masked[mask_r].to('m/s').value)
+                vr[i] = np.mean(vr_masked[mask_r].to('m/s').value) 
+                vz[i] = np.mean(vz_masked[mask_r].to('m/s').value) 
+                r_err[i] = np.std(r_masked[mask_r].to('au').value)
+                vphi_err[i] = np.std(vphi_masked[mask_r].to('m/s').value)
+                dvphi_err[i] = np.std(dvphi_masked[mask_r].to('m/s').value)
+                vr_err[i] = np.std(vr_masked[mask_r].to('m/s').value)
+                vz_err[i] = np.std(vz_masked[mask_r].to('m/s').value)
+            return {'r': r * u.au, 'vphi': vphi * u.m/u.s, 'dvphi': dvphi * u.m/u.s, 'vr': vr * u.m/u.s, 'vz': vz * u.m/u.s,
+                    'r_err': r_err * u.au, 'vphi_err': vphi_err * u.m/u.s, 'dvphi_err': dvphi_err * u.m/u.s, 'vr_err': vr_err * u.m/u.s, 'vz_err': vz_err * u.m/u.s}
+
+if __name__ == '__main__':
+    path = r"C:\Users\jamiechang917\Desktop\fargo3d-vis\data"
+    data = Data(path, 1000, 1, 100.0*u.au, 1.0*u.M_sun)
+    # planet_phi_index = np.abs(data.coords['sph_phi'] - data.params['planet0']['y'][data.snap]*u.rad).argmin()
+    
+    import matplotlib.pyplot as plt
+
+    profiles = data.get_velocity_profiles(254, 0.0, 0.004, 
+                                        #   -0.01, 0.01,
+                                          fluid='gas', coordinate='cyl')
+
+    fig, ax = plt.subplots(figsize=(12, 4))
+    ax.plot(profiles['r'], profiles['dvphi'], label=r'$v_{phi}$')
+    ax.fill_between(profiles['r'].value, (profiles['dvphi']-profiles['dvphi_err']).value, (profiles['dvphi']+profiles['dvphi_err']).value, alpha=0.3)
+    ax.errorbar(profiles['r'], profiles['dvphi'], yerr=profiles['dvphi_err'], xerr=profiles['r_err'], alpha=0.5, color='gray', fmt='none')
+    
+    ax.axhline(0, color='k', linestyle='--')
+    ax.axvline(100, color='k', linestyle='--')
+    plt.show()
---
+++ fargo3d/visual.py
@@ -0,0 +1,139 @@
+from fargo3d.data import Data
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
+
+def plot_velocity_profiles(data:Data, r_size:int, zr_min:float, zr_max:float,
+                           phi_min=-np.pi, phi_max=np.pi, fluid='gas', coordinate='cyl'):
+    if coordinate != 'cyl':
+        raise NotImplementedError('Only cylindrical coordinates are supported')
+    
+    profiles = data.get_velocity_profiles(r_size=r_size, zr_min=zr_min, zr_max=zr_max, 
+                                          phi_min=phi_min, phi_max=phi_max, fluid=fluid, coordinate=coordinate)
+    
+    planet_r = data.params["planet0"]["x"][data.snap] * data.units["unit_length"]
+
+    fig, axes = plt.subplots(3, 1, figsize=(12, 10), sharex=True, dpi=150)
+    plt.subplots_adjust(hspace=0)
+    for i in range(len(axes)):
+        if i == 0:
+            axes[i].plot(profiles['r'], profiles['dvphi'], label=r'$\delta v_{\phi}$')
+            axes[i].fill_between(profiles['r'].value, (profiles['dvphi'] - profiles['dvphi_err']).value, (profiles['dvphi'] + profiles['dvphi_err']).value, alpha=0.5)            
+            axes[i].set_ylabel(r'$\mathbf{\delta v_{\phi}}$ [m/s]', fontsize=16, fontweight='bold')
+        elif i == 1:
+            axes[i].plot(profiles['r'], profiles['vr'], label=r'$\delta v_{r}$')
+            axes[i].fill_between(profiles['r'].value, (profiles['vr'] - profiles['vr_err']).value, (profiles['vr'] + profiles['vr_err']).value, alpha=0.5)
+            axes[i].set_ylabel(r'$\mathbf{\delta v_{r}}$ [m/s]', fontsize=16, fontweight='bold')
+        elif i == 2:
+            axes[i].plot(profiles['r'], profiles['vz'], label=r'$\delta v_{z}$')
+            axes[i].fill_between(profiles['r'].value, (profiles['vz'] - profiles['vz_err']).value, (profiles['vz'] + profiles['vz_err']).value, alpha=0.5)
+            axes[i].set_ylabel(r'$\mathbf{\delta v_{z}}$ [m/s]', fontsize=16, fontweight='bold')
+            axes[i].set_xlabel('r [AU]', fontsize=16, fontweight='bold')
+        axes[i].axhline(0, color='black', linestyle='--', alpha=0.5)
+        axes[i].axvline(planet_r.value, color='black', linestyle='--', alpha=0.5, label='planet')
+        axes[i].set_xlim(profiles['r'].min().value, profiles['r'].max().value)
+        axes[i].set_ylim(-max(np.abs(axes[i].get_ylim())), max(np.abs(axes[i].get_ylim()))) # symmetric y-axis
+        axes[i].grid(True, which='major', axis='y', color='black', linestyle='--', alpha=0.3)
+
+        # beautify
+        axes[i].xaxis.set_major_locator(MultipleLocator(50))
+        axes[i].xaxis.set_minor_locator(MultipleLocator(10))
+        axes[i].yaxis.set_major_locator(MultipleLocator(20))
+        axes[i].yaxis.set_minor_locator(MultipleLocator(5))
+        axes[i].tick_params(axis='both', direction='in', which='both', top=True, right=True, width=2)
+        for spine in axes[i].spines.values():
+            spine.set_linewidth(2)
+        axes[i].yaxis.set_label_coords(-0.05, 0.5) # align ylabels
+        
+
+        # custom
+        axes[0].yaxis.set_major_locator(MultipleLocator(100))
+        axes[0].yaxis.set_minor_locator(MultipleLocator(20))
+        axes[1].set_ylim(-50, 50)
+        axes[2].set_ylim(-50, 50)
+    plt.show(fig)
+    return
+
+def plot_velocity_map(data:Data, r_bin:int, z_bin:int, scale:float,
+                      phi_min=-np.pi, phi_max=np.pi,
+                      r_min=None, r_max=None,
+                      z_min=None, z_max=None,
+                      fluid='gas', coordinate='cyl'):
+    """
+    Plot the 2D velocity map in the r-z plane.
+    data: Data object
+    r_bin: int, the bin size in r direction for the quiver plot
+    z_bin: int, the bin size in z direction for the quiver plot
+    scale: float, the scale of the quiver plot, bigger scale produces smaller arrows
+    phi_min: float, the minimum phi angle when averaging the velocity field
+    phi_max: float, the maximum phi angle when averaging the velocity field
+    r_min: float, the minimum r value for the plot
+    r_max: float, the maximum r value for the plot
+    z_min: float, the minimum z value for the plot
+    z_max: float, the maximum z value for the plot
+    fluid: str, the fluid type to plot
+    coordinate: str, the coordinate system to plot (only 'cyl' is supported now)
+    """
+    if coordinate != 'cyl':
+        raise NotImplementedError('Only cylindrical coordinates are supported')
+    r_mesh, z_mesh = data.mesh["cyl_r"], data.mesh["cyl_z"]
+    vr_mesh, vz_mesh = data.fluids["velocity"][fluid]["cyl_r"], data.fluids["velocity"][fluid]["cyl_z"]
+    phi_mask = (data.coords["sph_phi"].value >= phi_min) & (data.coords["sph_phi"].value <= phi_max)
+    r_mesh_masked, z_mesh_masked, vr_mesh_masked, vz_mesh_masked = r_mesh[:, :, phi_mask], z_mesh[:, :, phi_mask], vr_mesh[:, :, phi_mask], vz_mesh[:, :, phi_mask]
+    r_mesh_flatten, z_mesh_flatten, vr_mesh_flatten, vz_mesh_flatten = np.mean(r_mesh_masked, axis=2), np.mean(z_mesh_masked, axis=2), np.mean(vr_mesh_masked, axis=2), np.mean(vz_mesh_masked, axis=2)
+    # normalize vr, vz
+    vrz_mesh_flatten = np.sqrt(vr_mesh_flatten**2 + vz_mesh_flatten**2)
+    vr_mesh_norm = vr_mesh_flatten / vrz_mesh_flatten
+    vz_mesh_norm = vz_mesh_flatten / vrz_mesh_flatten
+
+    density_mesh = data.fluids["density"][fluid]
+    density_mesh_masked = density_mesh[:, :, phi_mask]
+    density_mesh_flatten = np.mean(density_mesh_masked, axis=2)
+
+    fig, ax = plt.subplots(1, 1, figsize=(12, 4), dpi=150)
+    pcm = ax.pcolormesh(r_mesh_flatten.value, z_mesh_flatten.value, vz_mesh_flatten.value, cmap='coolwarm', shading='auto', vmax=20, vmin=-20)
+    cbar = fig.colorbar(pcm, ax=ax, orientation='vertical', pad=0.0, extend='both', label=r'$\delta v_{z}$ [m/s]')
+    # ax.pcolormesh(r_mesh_flatten.value, z_mesh_flatten.value,  np.log10(density_mesh_flatten.value), cmap='jet', shading='auto', vmin=-17, vmax=-16)
+    quiver = ax.quiver(
+              r_mesh_flatten.value[::z_bin, ::r_bin], z_mesh_flatten.value[::z_bin, ::r_bin],
+              vr_mesh_norm.value[::z_bin, ::r_bin],   vz_mesh_norm.value[::z_bin, ::r_bin],
+              vrz_mesh_flatten.value[::z_bin, ::r_bin],
+              scale=scale, scale_units='inches', alpha=0.8, zorder=100, cmap='rainbow', clim=(0, 100))
+    # cbar = fig.colorbar(quiver, ax=ax, orientation='vertical', pad=0.0, extend='both', label=r'$\delta v_{r}$ [m/s]')
+
+    ax.set_xlabel('r [AU]', fontsize=16, fontweight='bold')
+    ax.set_ylabel('z [AU]', fontsize=16, fontweight='bold')
+
+
+    # z/r
+    for zr in [0.1, 0.2, 0.3, 0.4, 0.5]:
+        r = np.linspace(0.0, r_mesh_flatten.max().value, 100)
+        z = r * zr
+        ax.plot(r, z, color='gray', linestyle='-', alpha=0.5)
+
+
+    # beautify
+    ax.xaxis.set_major_locator(MultipleLocator(50))
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    ax.yaxis.set_major_locator(MultipleLocator(20))
+    ax.yaxis.set_minor_locator(MultipleLocator(5))
+    ax.tick_params(axis='both', direction='in', which='both', top=True, right=True, width=2)
+    for spine in ax.spines.values():
+        spine.set_linewidth(2)
+    ax.yaxis.set_label_coords(-0.05, 0.5) # align ylabels
+
+    if r_min is not None and r_max is not None:
+        ax.set_xlim(r_min, r_max)
+    else:
+        ax.set_xlim(0, r_mesh_flatten.max().value)
+    if z_min is not None and z_max is not None:
+        ax.set_ylim(z_min, z_max)
+    else:
+        ax.set_ylim(0.0, z_mesh_flatten.max().value)
+    
+    
+    plt.show(fig)
+
+
+    pass
